cf3d06b7a64de293983b3a72935363977760ff9f
ajfarzz <98690108+ajfarzz@users.noreply.github.com>
2022-03-03 23:16:14 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-// On install - caching the application shell
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      // cache any static files that make up the application shell
-      return cache.add('index.html');
-    })
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/js/app.js'
+        ])
+      })
   );
+  return self.clients.claim();
 });
 
-// On network request
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      //If response found return it, else fetch again
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(res) {
+        return res;
+      })
   );
 });
